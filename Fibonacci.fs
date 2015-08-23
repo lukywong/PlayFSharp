0e867910558e438cee291bab1706bb0f46489007
lukywong <lukywong@gmail.com>
2015-08-24 00:42:34 +0800

    Reduce one line code

--- Fibonacci.fs
+++ Fibonacci.fs
@@ -1,8 +1,7 @@
 let rec fib x =
   match x with
     | x when x < 0 -> failwith "value must be greater than 0"
-    | 0 -> 0
-    | 1 -> 1
+    | x when x < 2 -> x
     | x -> fib (x - 1) + fib (x - 2)
 
 fib 9 |> printfn "fib(9)=%d"
